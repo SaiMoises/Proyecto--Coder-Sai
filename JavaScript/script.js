ef1a96ad374ed8942cf965a188aab8c5fd34281a
Sai Cedeno <saijerry03@gmail.com>
2021-11-16 22:58:42 -0300

    ProyectoFinal

---
+++ JavaScript/script.js
@@ -0,0 +1,42 @@
+// Proyecto final
+
+class DatosUserFormulario {
+
+    constructor(nombre,apellido,cursoDeInteres,email,pais){
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.cursoDeInteres = cursoDeInteres;
+		this.email = email;
+		this.pais = pais;
+	
+	}
+
+  seleccionaPais() { 
+      if (this.pais == "ARGENTINA" || this.pais == ""){
+          console.log("Gracias por tu interes en el curso " + this.cursoDeInteres +" tenemos excelentes opciones para ti, te enviaremos la informacion al " + email)
+      }else {
+          console.log(" Gracias por elegirnos " + usuarioNuevo.nombre+" "+  usuarioNuevo.apellido +  ", Increible te enviaremos informacion a tu mail " + this.email)
+      }
+  }
+    
+}
+let nombre = prompt("Ingresa tu nombre")
+let apellido = prompt("Ingresa tu apellido")
+let cursoDeInteres = prompt("Ingresa tu curso de interes  para recibir informacion ")
+let email = prompt("Ingresa tu mail para recibir informacion ")
+let pais = prompt("Ingresa tu pais ").toUpperCase()
+ 
+const usuarioNuevo = new DatosUserFormulario(nombre,apellido,cursoDeInteres,email,pais)
+
+
+// console.log ( " Gracias por elegirnos " + usuarioNuevo.nombre + usuarioNuevo.apellido + " te enviaremos informacion sobre el curso de " + usuarioNuevo.cursoDeInteres + " a " + usuarioNuevo.email)
+ usuarioNuevo.seleccionaPais()
+
+
+
+
+
+
+
+
+
